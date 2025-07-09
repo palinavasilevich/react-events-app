b046c52e9561e4fb621084b25009cfd491f4557d
pvslvch <polina.vasilevichh@gmail.com>
2025-07-09 13:41:12 +0200

    feat: implement data validation in the newsletter subscription form

--- frontend/src/components/NewsletterSignup/NewsletterSignup.jsx
+++ frontend/src/components/NewsletterSignup/NewsletterSignup.jsx
@@ -1,8 +1,10 @@
+import { useEffect, useRef } from "react";
 import { useFetcher } from "react-router-dom";
+
 import classes from "./NewsletterSignup.module.css";
-import { useEffect } from "react";
 
 function NewsletterSignup() {
+  const formRef = useRef();
   const fetcher = useFetcher();
   const { data, state } = fetcher;
 
@@ -10,20 +12,30 @@ function NewsletterSignup() {
     if (state === "idle" && data && data.message) {
       window.alert(data.message);
     }
+
+    if (state === "idle" && data && data.ok) {
+      formRef.current?.reset();
+    }
   }, [data, state]);
 
   return (
     <fetcher.Form
+      ref={formRef}
       method="post"
       action="/newsletter"
       className={classes.newsletter}
     >
-      <input
-        type="email"
-        placeholder="Sign up for newsletter..."
-        aria-label="Sign up for newsletter"
-      />
-      <button>Sign up</button>
+      <div className={classes.container}>
+        <input
+          type="email"
+          name="email"
+          placeholder="Sign up for newsletter..."
+          aria-label="Sign up for newsletter"
+        />
+
+        <button>Sign up</button>
+      </div>
+      {data?.error && <p className={classes.errorText}>{data.error}</p>}
     </fetcher.Form>
   );
 }
--- frontend/src/pages/Newsletter/actions/newsletterAction.js
+++ frontend/src/pages/Newsletter/actions/newsletterAction.js
@@ -2,7 +2,13 @@ export async function action({ request }) {
   const data = await request.formData();
   const email = data.get("email");
 
+  console.log(data);
+
+  if (!email) {
+    return { ok: false, error: "Please enter your email address." };
+  }
+
   // send to backend newsletter server ...
   console.log(email);
-  return { message: "Signup successful!" };
+  return { ok: true, message: "Signup successful!" };
 }
