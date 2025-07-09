a43737883dd82720094916837312a1ca37595dad
pvslvch <polina.vasilevichh@gmail.com>
2025-07-09 11:08:45 +0200

    feat: implement deferred data fetching in the Events component

--- frontend/src/pages/Events.jsx
+++ frontend/src/pages/Events.jsx
@@ -1,21 +1,23 @@
-import { useLoaderData } from "react-router-dom";
+import { Suspense } from "react";
+import { Await, useLoaderData } from "react-router-dom";
 
 import { EventsList } from "../components";
 
 function EventsPage() {
-  const data = useLoaderData();
-  const events = data.events;
+  const { events } = useLoaderData();
 
   return (
-    <>
-      <EventsList events={events} />
-    </>
+    <Suspense fallback={<p>Loading...</p>}>
+      <Await resolve={events}>
+        {(loadedEvents) => <EventsList events={loadedEvents} />}
+      </Await>
+    </Suspense>
   );
 }
 
 export default EventsPage;
 
-export async function loader() {
+async function loadEvents() {
   const response = await fetch("http://localhost:8080/events");
 
   if (!response.ok) {
@@ -23,6 +25,13 @@ export async function loader() {
       status: 500,
     });
   } else {
-    return response;
+    const resData = await response.json();
+    return resData.events;
   }
+}
+
+export async function loader() {
+  return {
+    events: loadEvents(),
+  };
 }
