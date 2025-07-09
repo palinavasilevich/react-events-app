5df3e587334acb7e0df2dde07af712b5fdfe4032
pvslvch <polina.vasilevichh@gmail.com>
2025-07-09 11:31:28 +0200

    feat: implement HomePage component

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -1,5 +1,11 @@
+import { PageContent } from "../components";
+
 function HomePage() {
-  return <h1>HomePage</h1>;
+  return (
+    <PageContent title="Welcome!">
+      <p>Browse all our amazing evens!</p>
+    </PageContent>
+  );
 }
 
 export default HomePage;
